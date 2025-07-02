133e8f156ca7e6bb8eef180c9f23d2dea493017c
aakashtripathi306 <aakash.tripathi.mca@ghrua.edu.in>
2025-07-02 16:46:24 +0530

    resume name updated

--- src/data/navigationRoutes.ts
+++ src/data/navigationRoutes.ts
@@ -4,5 +4,5 @@ export const routes: NavbarRoutes = [
   { title: "Home", href: "/" },
   { title: "About", href: "/about" },
   { title: "Projects", href: "/projects" },
-  { title: "Resume", href: "/Aakash%20Tripathi(Full-stack).pdf" }, // direct to PDF
+  { title: "Resume", href: "/Aakash_Tripathi_Resume.pdf" }, // direct to PDF
 ];
